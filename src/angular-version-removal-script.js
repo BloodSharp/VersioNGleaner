d35bb2808b7065c45aa09d4550062d4cc53d3d72
Agustín Alejandro dos Santos <a.dossantos@buenosaires.gob.ar>
2024-05-28 10:57:39 -0300

    Fix: Translation from 'Spanish' to 'English' language

--- src/angular-version-removal-script.js
+++ src/angular-version-removal-script.js
@@ -1,48 +1,76 @@
 const fs = require("fs");
 var path = require("path");
 
-function removeAngularVersionOnEveryFile(directorio, fakeVersion) {
-  const files = fs.readdirSync(directorio);
+/**
+ * Removes the Angular's version on each file which ends with **_.html_** and **_.js_** extension at the specified directory path recursively.
+ * @param {String} directoryPath The base directory path which contains the Angular files built using the ng command line utility.
+ * @param {String} fakeVersion A fake version to replace the existing actual version.
+ * @param {Boolean} shouldAvoidShowcasingAngularVersionInformation If it's true, then strip the **_ng-version_** information, otherwise
+ * the **_ng-version_** information will be replaced using the **_fakeVersion_** information from the previous parameter.
+ */
+function removeAngularVersionOnEveryFile(
+  directoryPath,
+  fakeVersion,
+  shouldAvoidShowcasingAngularVersionInformation
+) {
+  const files = fs.readdirSync(directoryPath);
 
-  files.forEach((archivo) => {
-    const fullPath = path.join(directorio, archivo);
+  files.forEach((fileName) => {
+    const fullPath = path.join(directoryPath, fileName);
     const routeStatus = fs.statSync(fullPath);
 
     if (routeStatus.isDirectory()) {
       removeAngularVersionOnEveryFile(fullPath);
-    } else if (fullPath.endsWith(".js") || fullPath.endsWith(".html")) {
+    } else if (
+      fullPath.toLowerCase().endsWith(".js") ||
+      fullPath.toLowerCase().endsWith(".html")
+    ) {
       let fileContent = fs.readFileSync(fullPath, "utf8");
       /*
-      Expresión regular que corresponde al tag "app-root" del html:
+      Regular expression matching the "app-root" tag from html files:
       /ng-version=["']\d+\.\d+\.\d+["']/g
       
-      Explicación:
-      ng-version: busca el texto “ng-version”.
-      ["']: busca una comilla simple (') o doble (").
-      \d+\.\d+\.\d+: busca tres números separados por puntos.
-      ["']: busca una comilla simple (') o doble (") que cierra la versión.
-      /g: hace que la búsqueda sea global en todo el texto.
+      Explanation:
+      ng-version: Search the text “ng-version”.
+      ["']: Find either a single (') or double quote o doble (").
+      \d+\.\d+\.\d+: Find 3 numbers split by dots.
+      ["']: Find either a single (') or double quote o doble (") which closes the version.
+      /g: Specifies the search should be global at the whole file.
       */
-      fileContent = fileContent.replaceAll(
-        /ng-version=["']\d+\.\d+\.\d+["']/g,
-        `ng-version="${fakeVersion}"`
-      );
+      if (shouldAvoidShowcasingAngularVersionInformation) {
+        fileContent = fileContent.replaceAll(
+          /ng-version=["']\d+\.\d+\.\d+["']/g,
+          ""
+        );
+      } else {
+        fileContent = fileContent.replaceAll(
+          /ng-version=["']\d+\.\d+\.\d+["']/g,
+          `ng-version="${fakeVersion}"`
+        );
+      }
       /*
-      Expresión regular que corresponde a algunas de las funciones de JavaScript que están dentro de los chunks:
+      Regular expression matching some inside JavaScript functions within the chunks files:
       
       /\["ng-version",\s*"\d+\.\d+\.\d+"\]/g
-      Explicación:
-      \[ y \]: Coinciden con los caracteres “[” y “]” respectivamente.
-      "ng-version": Coincide con el texto “ng-version”.
-      ,: Coincide con una coma.
-      \s*: Coincide con cualquier cantidad de espacios en blanco (incluyendo ninguno).
-      "\d+\.\d+\.\d+": Coincide con una cadena que contiene tres números separados por puntos. \d+ coincide con uno o más dígitos, y \. coincide con un punto.
-      /g: Es una bandera que indica que la búsqueda debe ser global, es decir, debe encontrar todas las coincidencias en lugar de detenerse después de la primera.
+      Explanation:
+      \[ y \]: Matches all information between the characters “[” and “]”.
+      "ng-version": Search the text “ng-version”.
+      ,: Finds a comma.
+      \s*: Matches any blank spaces.
+      "\d+\.\d+\.\d+": Finds a string which contains 3 numbers (1 or more digits) split by dots.
+      /g: Specifies the search should be global at the whole file.
       */
-      fileContent = fileContent.replaceAll(
-        /\["ng-version",\s*"\d+\.\d+\.\d+"\]/g,
-        `["ng-version","${fakeVersion}"]`
-      );
+      if (shouldAvoidShowcasingAngularVersionInformation) {
+        fileContent = fileContent.replaceAll(
+          /\["ng-version",\s*"\d+\.\d+\.\d+"\]/g,
+          `["",""]`
+        );
+      } else {
+        fileContent = fileContent.replaceAll(
+          /\["ng-version",\s*"\d+\.\d+\.\d+"\]/g,
+          `["ng-version","${fakeVersion}"]`
+        );
+      }
       fs.writeFileSync(fullPath, fileContent);
       //console.log(`File: ${fullPath}\n`); //fileContent: ${fileContent}\n`);
     }
@@ -51,5 +79,6 @@ function removeAngularVersionOnEveryFile(directorio, fakeVersion) {
 
 removeAngularVersionOnEveryFile(
   path.resolve(__dirname, "../source/dist/"),
-  "h4ck3rm4n - 1337"
+  "h4ck3rm4n - 1337",
+  true
 );
